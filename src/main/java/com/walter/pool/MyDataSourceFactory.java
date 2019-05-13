3ee88d1eb8e645485eeebc32bcf5ec5869770127
git-walter <15221024927@163.com>
2019-05-14 06:28:07 +0800

    Mybatis_Druid使用Druid的配置文件

--- src/main/java/com/walter/pool/MyDataSourceFactory.java
+++ src/main/java/com/walter/pool/MyDataSourceFactory.java
@@ -5,29 +5,36 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import org.apache.ibatis.datasource.DataSourceFactory;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Properties;
+
 //写给mybatis调用
 public class MyDataSourceFactory implements DataSourceFactory {
 
-    private Properties properties;
     private static DruidDataSource dataSource;
 
     @Override
     public void setProperties(Properties properties) {
-        this.properties = properties;
+        //不用参数里的properties,来自mybatis.xml
     }
 
     @Override
     public DataSource getDataSource() {
         try {
-            dataSource = (DruidDataSource)DruidDataSourceFactory.createDataSource(properties);
+            Properties properties = new Properties();
+            properties.load(MyDataSourceFactory.class.getClassLoader()
+                    .getResourceAsStream("dbconfig.properties"));
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return dataSource;
     }
 
-    public static void dataSourceStatus(){
+    public static void dataSourceStatus() {
         System.out.println(dataSource);
         dataSource.setMaxActive(21);
         //作用同配置文件
